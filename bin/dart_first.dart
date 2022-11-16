955d8440a5afe63852be1b5686575b6b75defd82
n-kev1n <kevin_@tuta.io>
2022-11-16 21:02:27 +0100

    added variables

--- bin/dart_first.dart
+++ bin/dart_first.dart
@@ -1,5 +1,10 @@
-import 'package:dart_first/dart_first.dart' as dart_first;
+//import 'package:dart_first/dart_first.dart' as dart_first;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_first.calculate()}!');
+  String car = 'BMW';
+  int num1 = 8;
+  int num2 = 12;
+  //print('Hello world: ${dart_first.calculate()}!');
+  print('My car is ${car}');
+  print('The Sum of ${num1} and ${num2} is ${num1 + num2}');
 }
